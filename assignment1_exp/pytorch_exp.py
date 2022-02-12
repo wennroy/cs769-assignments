5d22a5cae7dbd71db7a92e2581decdda8206b48d
wennroy <wennroy@gmail.com>
2022-02-12 17:21:52 -0600

    Add assignment1_exp

---
+++ assignment1_exp/pytorch_exp.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+import os
+import time
+import random
+import torch
+import torch.nn as nn
+import model as mn
+import numpy as np
+import argparse
+from vocab import Vocab
+
